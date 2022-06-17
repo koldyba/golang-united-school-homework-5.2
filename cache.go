d39c2b89798fa5265bcd559668ead213c075a8ec
Sergey Koldyba <sergey.koldyba@dataart.com>
2022-06-17 17:45:59 +0200

    add solution

--- cache.go
+++ cache.go
@@ -2,22 +2,54 @@ package cache
 
 import "time"
 
+type record struct {
+	key        string
+	value      string
+	isExpired  bool
+	expireTime time.Time
+}
+
+func (r *record) expire() (ie bool) {
+	t := time.Now()
+	r.isExpired = t.After(r.expireTime)
+	return
+}
+
 type Cache struct {
+	Record []record
 }
 
 func NewCache() Cache {
 	return Cache{}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c *Cache) Get(key string) (string, bool) {
+	for _, r := range c.Record {
+		if r.key == key && !r.expire() {
+			return r.value, true
+		}
+	}
+	return "", false
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	r := record{key: key, value: value, isExpired: false, expireTime: time.Time{}}
+	c.Record = append(c.Record, r)
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	rs := []string{}
+	for _, r := range c.Record {
+		r.expire()
+		if !r.expire() {
+			rs = append(rs, r.key)
+		}
+	}
+	return rs
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	r := record{key: key, value: value, isExpired: false, expireTime: deadline}
+	r.expire()
+	c.Record = append(c.Record, r)
 }
